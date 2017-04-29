4c15cbeff97fb617a21e79bfc18ccc6c929556b7
bessefc <bessefc@localhost>
2017-04-29 12:36:23 +0200

    déplacement du calcul du zoom dans le modele

--- CablageGit/src/fr/enac/iessa16/cablage/controller/ControleurSouris.java
+++ CablageGit/src/fr/enac/iessa16/cablage/controller/ControleurSouris.java
@@ -7,8 +7,6 @@ import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 
 import fr.enac.iessa16.cablage.model.Modele;
-import fr.enac.iessa16.cablage.view.PanneauDessinGraphe;
-import fr.enac.iessa16.cablage.view.ParametresFenetre;
 
 
 /**
@@ -94,31 +92,14 @@ public class ControleurSouris implements MouseListener, MouseMotionListener, Mou
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
 
-		// on récupère la rotation de la molette (le "-" permet de modifier le
-		// sens de rotation pour zoomer/dézoomer)
-		int delta = -5 * e.getWheelRotation();
-		
 		// on récupére la position de la souris pour zoomer sur la souris
 		int xs = e.getX();
 		int ys = e.getY();
-
-		// on calcule la nouvelle échelle pour la vue
-		double newEchelle = ParametresFenetre.echelle * 
-				 Math.pow(1.01, delta);
 		
-		// on calcule les nouveaux offset en fonction de la position de la souris
-		double newOffsetX = ParametresFenetre.offsetX
-				+ PanneauDessinGraphe.getRealCoordX(xs) / ParametresFenetre.ECHELLE_BASE
-				* (ParametresFenetre.echelle - newEchelle);
-		double newOffsetY = ParametresFenetre.offsetY
-				- PanneauDessinGraphe.getRealCoordY(ys) / ParametresFenetre.ECHELLE_BASE
-				* (ParametresFenetre.echelle - newEchelle);
+		// on récupére la rotation de la molette
+		int rotation = e.getWheelRotation();
 		
-	
-		ParametresFenetre.echelle = newEchelle;
-		ParametresFenetre.offsetX = newOffsetX;
-		ParametresFenetre.offsetY = newOffsetY;
-				
-		modele.changement();
+		// on appelle la fonction zoom du modele
+		modele.zoom(xs, ys, rotation);
 	}
 }
