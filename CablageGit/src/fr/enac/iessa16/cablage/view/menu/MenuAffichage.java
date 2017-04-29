eebeb6a4a8f85283d6c01914a798a6b39b072ee8
hedidira <hedidira@localhost>
2017-04-30 00:41:53 +0200

    Changement MenuAffichage , integration des JSeparator

--- CablageGit/src/fr/enac/iessa16/cablage/view/menu/MenuAffichage.java
+++ CablageGit/src/fr/enac/iessa16/cablage/view/menu/MenuAffichage.java
@@ -1,7 +1,11 @@
 package fr.enac.iessa16.cablage.view.menu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 
 import fr.enac.iessa16.cablage.controller.Controleur;
 import fr.enac.iessa16.cablage.view.ParametresFenetre;
@@ -17,19 +21,20 @@ public class MenuAffichage extends JMenu {
 		this.add(optionCentrage);
 		optionCentrage.setActionCommand(ParametresFenetre.centrage);
 		optionCentrage.addActionListener(controleur.getControleurMenuAffichage());
-		
-		
+		this.addSeparator();
+	
 		
 		JMenuItem optionZoomer = new JMenuItem(ParametresFenetre.zoomPlus);
 		this.add(optionZoomer);
 		optionZoomer.setActionCommand(ParametresFenetre.zoomPlus);
 		optionZoomer.addActionListener(controleur.getControleurMenuAffichage());
+
 		
 		JMenuItem optionDezoomer = new JMenuItem(ParametresFenetre.zoomMoins);
-		this.add(optionDezoomer);
 		optionDezoomer.setActionCommand(ParametresFenetre.zoomMoins);
 		optionDezoomer.addActionListener(controleur.getControleurMenuAffichage());
-		
+		optionCentrage.setAccelerator(KeyStroke.getKeyStroke("-"));
+		this.add(optionDezoomer);
 		
 		
 		
